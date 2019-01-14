e4f715038a20e85e49fa4dd8ff1e034e8708c191
Samo Forus <samo.forus@touch4it.com>
2019-01-14 15:45:28 +0100

    Add PHP 7.3

--- drupal/build.sh
+++ drupal/build.sh
@@ -9,12 +9,20 @@ docker build ./php7.2-fpm-nginx-drupal8.6 \
 	-t touch4it/drupal-php-fpm-nginx:latest \
 	-f ./php7.2-fpm-nginx-drupal8.6/Dockerfile
 
+docker build ./php7.3-fpm-nginx-drupal8.6 \
+	-t touch4it/drupal-php-fpm-nginx:8.6-php7.3 \
+	-f ./php7.3-fpm-nginx-drupal8.6/Dockerfile
+
 docker build ./php7.2-fpm-nginx-drupal8.5 \
 	-t touch4it/drupal-php-fpm-nginx:8.5-php7.2 \
 	-t touch4it/drupal-php-fpm-nginx:8.5 \
 	-t touch4it/drupal-php-fpm-nginx:8.5.8 \
 	-t touch4it/drupal-php-fpm-nginx:8.5.8-1 \
 	-f ./php7.2-fpm-nginx-drupal8.5/Dockerfile
+
+docker build ./php7.3-fpm-nginx-drupal8.5 \
+	-t touch4it/drupal-php-fpm-nginx:8.5-php7.3 \
+	-f ./php7.3-fpm-nginx-drupal8.5/Dockerfile
 
 # Build development images
 
@@ -23,10 +31,18 @@ docker build ./php7.2-fpm-nginx-drupal8.6-dev \
 	-t touch4it/drupal-php-fpm-nginx:8.6-dev \
 	-f ./php7.2-fpm-nginx-drupal8.6-dev/Dockerfile
 
+docker build ./php7.3-fpm-nginx-drupal8.6-dev \
+	-t touch4it/drupal-php-fpm-nginx:8.6-php7.3-dev \
+	-f ./php7.3-fpm-nginx-drupal8.6-dev/Dockerfile
+
 docker build ./php7.2-fpm-nginx-drupal8.5-dev \
 	-t touch4it/drupal-php-fpm-nginx:8.5-php7.2-dev \
 	-t touch4it/drupal-php-fpm-nginx:8.5-dev \
 	-f ./php7.2-fpm-nginx-drupal8.5-dev/Dockerfile
+
+docker build ./php7.3-fpm-nginx-drupal8.5-dev \
+	-t touch4it/drupal-php-fpm-nginx:8.5-php7.3-dev \
+	-f ./php7.3-fpm-nginx-drupal8.5-dev/Dockerfile
 
 # Build Drupal console
 
@@ -41,18 +57,22 @@ docker push touch4it/drupal-php-fpm-nginx:latest
 docker push touch4it/drupal-php-fpm-nginx:8.6.5
 docker push touch4it/drupal-php-fpm-nginx:8.6
 docker push touch4it/drupal-php-fpm-nginx:8.6-php7.2
+docker push touch4it/drupal-php-fpm-nginx:8.6-php7.3
 
 docker push touch4it/drupal-php-fpm-nginx:8.5.8
 docker push touch4it/drupal-php-fpm-nginx:8.5
 docker push touch4it/drupal-php-fpm-nginx:8.5-php7.2
+docker push touch4it/drupal-php-fpm-nginx:8.5-php7.3
 
 # Deploy dev images
 
 docker push touch4it/drupal-php-fpm-nginx:8.6-dev
 docker push touch4it/drupal-php-fpm-nginx:8.6-php7.2-dev
+docker push touch4it/drupal-php-fpm-nginx:8.6-php7.3-dev
 
 docker push touch4it/drupal-php-fpm-nginx:8.5-dev
 docker push touch4it/drupal-php-fpm-nginx:8.5-php7.2-dev
+docker push touch4it/drupal-php-fpm-nginx:8.5-php7.3-dev
 
 # Deploy Docker console
 
