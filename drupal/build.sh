6cca62aa340097393a6dc538a79e9124ceb87054
Viktor Šulák <sulak@touch4it.com>
2019-02-14 15:32:23 +0100

    Update to Drupal 8.6.9

--- drupal/build.sh
+++ drupal/build.sh
@@ -5,7 +5,7 @@
 docker build ./php7.2-fpm-nginx-drupal8.6 \
 	-t touch4it/drupal-php-fpm-nginx:8.6-php7.2 \
 	-t touch4it/drupal-php-fpm-nginx:8.6 \
-	-t touch4it/drupal-php-fpm-nginx:8.6.7 \
+	-t touch4it/drupal-php-fpm-nginx:8.6.9 \
 	-t touch4it/drupal-php-fpm-nginx:latest \
 	-f ./php7.2-fpm-nginx-drupal8.6/Dockerfile
 
@@ -43,7 +43,7 @@ docker build ./console \
 
 docker push touch4it/drupal-php-fpm-nginx:latest
 
-docker push touch4it/drupal-php-fpm-nginx:8.6.7
+docker push touch4it/drupal-php-fpm-nginx:8.6.9
 docker push touch4it/drupal-php-fpm-nginx:8.6
 docker push touch4it/drupal-php-fpm-nginx:8.6-php7.2
 docker push touch4it/drupal-php-fpm-nginx:8.6-php7.3
--- drupal/php7.3-fpm-nginx-drupal8.6-dev/README.md
+++ drupal/php7.3-fpm-nginx-drupal8.6-dev/README.md
@@ -19,7 +19,7 @@ This repository is a source code for following Docker images:
 *   Latest release
     *   touch4it/drupal-php-fpm-nginx:latest
 *   Production images
-    *   8.6.7, 8.6, 8.6-php7.2, 8.6-php7.3
+    *   8.6.9, 8.6, 8.6-php7.2, 8.6-php7.3
     *   8.5.10, 8.5, 8.5-php7.2
 *   Development images
     *   8.6-dev
