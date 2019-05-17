d9a69c5c0561ce3789fa1a923e184d834f89c94b
Viktor Šulák <sulak@touch4it.com>
2019-05-17 17:40:01 +0200

    Rebuilt images

--- build.sh
+++ build.sh
@@ -2,42 +2,42 @@
 
 # Build production images
 
-docker build ./php7.1-apache \
+docker build --no-cache ./php7.1-apache \
 	-t touch4it/docker-php7:latest \
 	-t touch4it/docker-php7:php7.1-apache \
 	-f ./php7.1-apache/Dockerfile
 
-docker build ./php7.2-apache \
+docker build --no-cache ./php7.2-apache \
 	-t touch4it/docker-php7:php7.2-apache \
 	-f ./php7.2-apache/Dockerfile
 
-docker build ./php7.3-apache \
+docker build --no-cache ./php7.3-apache \
 	-t touch4it/docker-php7:php7.3-apache \
 	-f ./php7.3-apache/Dockerfile
 
-docker build ./php7.1-fpm-nginx \
+docker build --no-cache ./php7.1-fpm-nginx \
 	-t touch4it/docker-php7:php7.1-fpm-nginx \
 	-f ./php7.1-fpm-nginx/Dockerfile
 
-docker build ./php7.2-fpm-nginx \
+docker build --no-cache ./php7.2-fpm-nginx \
 	-t touch4it/docker-php7:php7.2-fpm-nginx \
 	-f ./php7.2-fpm-nginx/Dockerfile
 
-docker build ./php7.3-fpm-nginx \
+docker build --no-cache ./php7.3-fpm-nginx \
 	-t touch4it/docker-php7:php7.3-fpm-nginx \
 	-f ./php7.3-fpm-nginx/Dockerfile
 
 # Build development images
 
-docker build ./php7.1-fpm-nginx-dev \
+docker build --no-cache ./php7.1-fpm-nginx-dev \
 	-t touch4it/docker-php7:php7.1-fpm-nginx-dev \
 	-f ./php7.1-fpm-nginx-dev/Dockerfile
 
-docker build ./php7.2-fpm-nginx-dev \
+docker build --no-cache ./php7.2-fpm-nginx-dev \
 	-t touch4it/docker-php7:php7.2-fpm-nginx-dev \
 	-f ./php7.2-fpm-nginx-dev/Dockerfile
 
-docker build ./php7.3-fpm-nginx-dev \
+docker build --no-cache ./php7.3-fpm-nginx-dev \
 	-t touch4it/docker-php7:php7.3-fpm-nginx-dev \
 	-f ./php7.3-fpm-nginx-dev/Dockerfile
 
--- drupal/build.sh
+++ drupal/build.sh
@@ -2,51 +2,51 @@
 
 # Build production images
 
-docker build ./php7.3-fpm-nginx-drupal8.7 \
+docker build --no-cache ./php7.3-fpm-nginx-drupal8.7 \
 	-t touch4it/drupal-php-fpm-nginx:8.7-php7.3 \
 	-t touch4it/drupal-php-fpm-nginx:8.7 \
 	-t touch4it/drupal-php-fpm-nginx:8.7.1 \
 	-f ./php7.3-fpm-nginx-drupal8.7/Dockerfile
 
-docker build ./php7.2-fpm-nginx-drupal8.6 \
+docker build --no-cache ./php7.2-fpm-nginx-drupal8.6 \
 	-t touch4it/drupal-php-fpm-nginx:8.6-php7.2 \
 	-t touch4it/drupal-php-fpm-nginx:8.6 \
 	-t touch4it/drupal-php-fpm-nginx:8.6.16 \
 	-t touch4it/drupal-php-fpm-nginx:latest \
 	-f ./php7.2-fpm-nginx-drupal8.6/Dockerfile
 
-docker build ./php7.3-fpm-nginx-drupal8.6 \
+docker build --no-cache ./php7.3-fpm-nginx-drupal8.6 \
 	-t touch4it/drupal-php-fpm-nginx:8.6-php7.3 \
 	-f ./php7.3-fpm-nginx-drupal8.6/Dockerfile
 
-docker build ./php7.3-fpm-nginx-drupal8.5 \
+docker build --no-cache ./php7.3-fpm-nginx-drupal8.5 \
 	-t touch4it/drupal-php-fpm-nginx:8.5-php7.3 \
 	-f ./php7.3-fpm-nginx-drupal8.5/Dockerfile
 
 # Build development images
 
-docker build ./php7.3-fpm-nginx-drupal8.7-dev \
+docker build --no-cache ./php7.3-fpm-nginx-drupal8.7-dev \
 	-t touch4it/drupal-php-fpm-nginx:8.7-php7.3-dev \
 	-t touch4it/drupal-php-fpm-nginx:8.7-dev \
 	-f ./php7.3-fpm-nginx-drupal8.7-dev/Dockerfile
 
-docker build ./php7.2-fpm-nginx-drupal8.6-dev \
+docker build --no-cache ./php7.2-fpm-nginx-drupal8.6-dev \
 	-t touch4it/drupal-php-fpm-nginx:8.6-php7.2-dev \
 	-t touch4it/drupal-php-fpm-nginx:8.6-dev \
 	-f ./php7.2-fpm-nginx-drupal8.6-dev/Dockerfile
 
-docker build ./php7.3-fpm-nginx-drupal8.6-dev \
+docker build --no-cache ./php7.3-fpm-nginx-drupal8.6-dev \
 	-t touch4it/drupal-php-fpm-nginx:8.6-php7.3-dev \
 	-f ./php7.3-fpm-nginx-drupal8.6-dev/Dockerfile
 
-docker build ./php7.2-fpm-nginx-drupal8.5-dev \
+docker build --no-cache ./php7.2-fpm-nginx-drupal8.5-dev \
 	-t touch4it/drupal-php-fpm-nginx:8.5-php7.2-dev \
 	-t touch4it/drupal-php-fpm-nginx:8.5-dev \
 	-f ./php7.2-fpm-nginx-drupal8.5-dev/Dockerfile
 
 # Build Drupal console
 
-docker build ./console \
+docker build --no-cache ./console \
 	-t touch4it/drupal-php-fpm-nginx:console \
 	-f ./console/Dockerfile
 
